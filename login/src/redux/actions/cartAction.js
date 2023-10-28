92dae0b54799eabf4f66e4e991a19f102489cfdc
ayushjidhanta <ayushjidhanta@gmail.com>
2023-10-28 22:38:42 +0530

    change url

--- login/src/redux/actions/cartAction.js
+++ login/src/redux/actions/cartAction.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import * as actionType from "../constants/cartConstants";
-const URL = "http://localhost:5000";
-
+const URL = "";
 
 export const addToCart = (id, quantity) => async (dispatch) => {
   try {
@@ -9,20 +8,23 @@ export const addToCart = (id, quantity) => async (dispatch) => {
 
     // check if it data also in cart or not
     // const response = await saveCartDetails(data);
-  
+
     // if (response === 409) {
     //   console.log("Already in cart can't add one item two times....");
     //   return;
     // }
 
     console.log("Redux", JSON.parse(data));
-    
-    dispatch({ type: actionType.ADD_TO_CART, payload: { ...JSON.parse(data), quantity } });
+
+    dispatch({
+      type: actionType.ADD_TO_CART,
+      payload: { ...JSON.parse(data), quantity },
+    });
   } catch (error) {
     dispatch({ type: actionType.ADD_TO_CART_ERROR, payload: error.message });
   }
 };
 
 export const removeFromCart = (id) => (dispatch) => {
-   dispatch({type: actionType.REMOVE_FROM_CART, payload: id});
+  dispatch({ type: actionType.REMOVE_FROM_CART, payload: id });
 };
--- login/src/service/api.js
+++ login/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/";
+const url = "";
 export const signupuser = async (data) => {
   try {
     const response = await axios.post(`${url}signup/createuser`, data);
